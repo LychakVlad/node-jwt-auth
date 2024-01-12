1759a90910eb4ebc1ad93d90b26ee6880e8b30b0
Vladislav <lycakvladislav@gmail.com>
2024-01-12 17:53:15 -0500

    connect postgesql

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
   try {
     app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
