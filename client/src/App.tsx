8b59e91dc57fe907aa5867eaa47223f2d140c5c4
Vladislav <lycakvladislav@gmail.com>
2024-01-14 20:36:11 -0500

    add get users functions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,55 @@
+import { useContext, useEffect, useState } from 'react';
 import LoginForm from './components/LoginForm';
+import { Context } from './index';
+import { observer } from 'mobx-react-lite';
+import { IUser } from './models/IUser';
+import UserService from './services/UserService';
 
 function App() {
+  const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      store.checkAuth();
+    }
+  }, []);
+
+  console.log(users);
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!store.isAuth) {
+    return <LoginForm />;
+  }
+
   return (
     <div>
-      <LoginForm />
+      <h1>
+        {store.isAuth
+          ? `User authenticated  ${store.user.email}`
+          : 'PLEASE LOG IN'}
+      </h1>
+      <button onClick={() => store.logout()}>Log out</button>
+      <div>
+        <button onClick={getUsers}>Get users</button>
+      </div>
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default observer(App);
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -73,7 +73,7 @@ class UserController {
   async getUsers(req, res, next) {
     try {
       const users = await userService.getAllUsers();
-      return res.json(users);
+      return res.json(users.rows);
     } catch (error) {
       next(error);
     }
